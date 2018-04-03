bd0dffb84dbbd5d63358c7cbe40d8badd85ddec1
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-04-03 11:53:34 +0300

    added config

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var cors = require('cors');
+var nconf = require('nconf');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var fs = require('fs');
 
 var cv = require('./routes/cv');
 var projects = require('./routes/projects');
@@ -22,15 +24,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.set('location', 'public');
+// set up congifuration
+nconf.argv().env();
+nconf.file({ file: 'config.json' });
+nconf.defaults({
+  "http": {
+    "port": 3000
+  },
+  "data": {
+    "location": "public"
+  }
+});
+
+app.set('location', nconf.get('data:location'));
+
 app.set('json', path.join(app.get('location'), 'json'));
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// load app icon
+var faviconPath = path.join(__dirname, app.get('location'), 'favicon.ico');
+if (fs.existsSync(faviconPath)) {
+  app.use(favicon(faviconPath));
+}
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, app.get('location'))));
 
 app.use(cors());
 
@@ -44,18 +64,18 @@ app.use('/entities', entities);
 app.use('/ui', ui);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/ui.js
+++ routes/ui.js
@@ -8,6 +8,11 @@ const obfuscator = require('../javascripts/obfuscator');
 router.get('/', function (req, res) {
     var data = require(path.resolve(req.app.get('json'), 'ui.json'));
 
+    try {
+        data.Disclaimer.text = data.Disclaimer.text.join(' ');
+    } catch (e) {
+    }
+
     data = obfuscator.obfuscateWhenNeeded(req.app, data);
 
     res.send(data);
