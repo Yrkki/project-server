e45c6d4b262e8a2d9c89c81a1917152d2e04d8b5
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-04-25 12:34:02 +0300

    cached data for a week

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, app.get('location'))));
+app.use(express.static(path.join(__dirname, app.get('location')), { maxAge: '1w' }));
 
 app.use(cors());
 
