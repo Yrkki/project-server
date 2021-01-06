e5279e3d6b7f7e74eba85aded96a39e949e88ab0
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-01-06 22:43:50 +0200

    fix(cdn): better detect the redirect response body

--- routes/json.js
+++ routes/json.js
@@ -70,7 +70,7 @@ function respond(req, currentPath, res, data) {
 
   try {
     // prep amd json package text data like e.g. redirect info message
-    if (typeof data === 'string') {
+    if (!data.startsWith('{')) {
       if (process.env.CV_GENERATOR_PROJECT_SERVER_ENCRYPTER === 'decrypt') {
         data = encrypter.encrypt(data);
       } else if (process.env.CV_GENERATOR_PROJECT_SERVER_ENCRYPTER === 'encrypt') {
