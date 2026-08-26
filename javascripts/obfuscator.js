b6a78b4a524c6f991f9638026db156d65a9b351d
Jorich <35774231+Yrkki@users.noreply.github.com>
2026-08-26 16:33:30 +0300

    test(vulnerabilities): change the urlExists vulnerable dependency to urlExist

--- javascripts/obfuscator.js
+++ javascripts/obfuscator.js
@@ -1,31 +1,29 @@
 const path = require('path');
-const urlExists = require('url-exists');
 const fs = require('fs');
 
 const li = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
 
-function obfuscateWhenNeeded(currentPath, data) {
+async function obfuscateWhenNeeded(currentPath, data) {
     console.debug('obfuscator.js: obfuscateWhenNeeded: Obfuscating...');
+
+    // Dynamically import ES Module when running in a CommonJS context
+    const urlExists = (await import('url-exist')).default;
 
     const a = 'auth.json';
     if (currentPath.startsWith('http')) {
         const authUrl = currentPath + '/' + a;
         console.debug('obfuscator.js: obfuscateWhenNeeded: Checking url:', authUrl);
 
-        const urlExistsPromise = url =>
-            new Promise((resolve, reject) =>
-                urlExists(url, (err, exists) =>
-                    err ? reject(err) : resolve(exists)));
-
-        return urlExistsPromise(authUrl).then(exists =>
-            exists ? data : obfuscate(data));
+        // Directly await the Promise-based function
+        const exists = await urlExists(authUrl);
+        return exists ? data : obfuscate(data);
     } else {
         const authPath = path.resolve(currentPath, a);
         console.debug('obfuscator.js: obfuscateWhenNeeded: Checking path:', authPath);
         if (!fs.existsSync(authPath)) {
             data = obfuscate(data);
         }
-        return new Promise(resolve => resolve(data));
+        return data;
     }
 }
 
